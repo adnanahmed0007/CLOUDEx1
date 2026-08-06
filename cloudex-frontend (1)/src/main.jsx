c39f678eba2747cbefc2b960f39fb359fe40ec54
adnanahmed0007 <139644036+adnanahmed0007@users.noreply.github.com>
2026-08-07 01:01:08 +0530

    Refactor ReactDOM render method for main.jsx

--- cloudex-frontend (1)/src/main.jsx
+++ cloudex-frontend (1)/src/main.jsx
@@ -1,20 +1,22 @@
-import React from "react";
+ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
- import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider } from "./context/ThemeContext";
 
-<React.StrictMode>
-  <ThemeProvider>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>
-</React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <BrowserRouter
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
